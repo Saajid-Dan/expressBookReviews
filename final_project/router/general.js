6c8482425ec8cfcbeb7df783f861b563a3f9e6c2
Saajid-Dan <saajid.dan@hotmail.com>
2024-05-12 02:10:50 -0400

    Improving the scope of Tasks 1-4 using Promises or Async-Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -14,7 +14,7 @@ const doesExist = (username)=>{
       return false;
     }
   }
-  
+
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -31,43 +31,70 @@ public_users.post("/register", (req,res) => {
   });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+public_users.get('/',async function (req, res) {
+    await res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    if (books[isbn]){
-        res.send(books[isbn])
-    }
-    else{
-        res.send('Can\'t find the ISBN');
-    }
- });
-  
+    const myPromise1 = new Promise((resolve, reject)=>{
+        const isbn = req.params.isbn;
+        if (books[isbn]){
+            resolve(books[isbn]);
+        }
+        else{
+            reject('Can\'t find the ISBN');
+        }
+      });
+      
+    myPromise1.then((resp)=>{
+        res.status(200).json(resp)
+    }).catch(
+        err=> res.status(403).json({error: err})
+    )
+
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    const getISBN = Object.values(books).filter(value => value.author === author);
-    if (getISBN){
-        res.send(getISBN);
-    }
-    else{
-        res.send('Can\'t find the Author');
-    }
+    const myPromise2 = new Promise((resolve, reject)=>{
+        const author = req.params.author;
+        const getISBN = Object.values(books).filter(value => value.author === author);
+        if (getISBN){
+            resolve(getISBN);
+        }
+        else{
+            reject('Can\'t find the Author');
+        }
+    });
+      
+    myPromise2.then((resp)=>{
+        res.status(200).json(resp)
+    }).catch(
+        err=> res.status(403).json({error: err})
+    )
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    const getISBN = Object.keys(books).find(key => books[key].title === title);
-    if (getISBN){
-        res.send(books[getISBN]);
-    }
-    else{
-        res.send('Can\'t find the Title');
-    }
+    const myPromise3 = new Promise((resolve, reject)=>{
+        const title = req.params.title;
+        const getISBN = Object.keys(books).find(key => books[key].title === title);
+        if (getISBN){
+            resolve(books[getISBN]);
+        }
+        else{
+            reject('Can\'t find the Title');
+        }
+    });
+      
+    myPromise3.then((resp)=>{
+        res.status(200).json(resp)
+    }).catch(
+        err=> res.status(403).json({error: err})
+    )
+    
 });
 
 //  Get book review
